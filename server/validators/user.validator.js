289da260251fe1ae5a473413b68c8adbcf351810
Mekha0 <mehreenk767@gmail.com>
2026-04-06 19:18:41 -0400

    formatting

--- server/validators/user.validator.js
+++ server/validators/user.validator.js
@@ -3,9 +3,7 @@ import { ObjectId } from "mongodb";
 
 const signupSchema = z.object({
     firstName: z.string().min(1, "First name is required"),
-
     username: z.string().min(5, "Username must be at least 3 characters"),
-
     email: z.email("Invalid email"),
     password: z.string().min(6, "Password must be 6 characters long")
 });

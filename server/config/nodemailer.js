107647b6a9643e5b638dcd017cdc377c96847f2a
Mekha0 <mehreenk767@gmail.com>
2026-04-08 02:54:36 -0400

    set up transporter for emails

---
+++ server/config/nodemailer.js
@@ -0,0 +1,22 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config({ path: __dirname + "/.env" });
+
+// Create a transporter object using SMTP
+const transporter = nodemailer.createTransport({
+  service: "gmail", 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+// Verify connection configuration
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("Email configuration error: ", error);
+  } else {
+    console.log("Email server is ready to send messages!");
+  }
+});
+
+module.exports = transporter;

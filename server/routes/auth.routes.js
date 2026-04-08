b9c2be252191dbe36f995fe61c9a0753f1d0fcfe
Mekha0 <mehreenk767@gmail.com>
2026-04-08 02:55:44 -0400

    route for email activation added

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login } = require("../controllers/auth.controller");
+const { signup, login, activateEmail } = require("../controllers/auth.controller");
+
 
 // Signup route - mk
 router.post("/signup", signup);
+
+// email activation route
+router.get("/activate/:token", activateEmail)
 
 // Login endpoint
 router.post("/login", login);

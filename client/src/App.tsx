4b36c2a8479223468bb0c4c008f3bae5a0ca6e25
noamc-jpg <noamcschool@gmail.com>
2026-04-07 21:24:49 -0400

    login/signup work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
 import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      <LandingPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+
+      </Routes>
+    </Router>
   )
 }
 

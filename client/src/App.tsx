2c3bec04a93fb4d4fa67ac03a81d034f7ab37c75
noamc-jpg <noamcschool@gmail.com>
2026-04-08 01:21:11 -0400

    login/signup pages, linked via header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SignupPage from './pages/SignupPage'
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
 
         <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/signup" element={<SignupPage />} />
 
       </Routes>
     </Router>
--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -2,21 +2,27 @@ import SiteHeader from "../components/SiteHeader/SiteHeader";
 import SiteFooter from "../components/SiteFooter/SiteFooter";
 import "../auth.css";
 
-export default function LoginPage() {
+export default function SignupPage() {
     return (
         <div className="auth-page-wrapper">
             <SiteHeader />
             
             <main className="auth-main">
                 <section className="auth-card">
-                    <h1>Log In</h1>
+                    <h1>Sign Up</h1>
                     
                     <form>
+                        <label className="input-label" htmlFor="tel">First Name</label>
+                        <input className="auth-input" type="text" id="firstname" name="firstname" required />
+
+                        <label className="input-label" htmlFor="email">Email</label>
+                        <input className="auth-input" type="email" id="email" name="email" required />
+
                         <label className="input-label" htmlFor="username">Username</label>
-                        <input className="auth-input" type="text" id="username" name="username" />
+                        <input className="auth-input" type="text" id="username" name="username" required />
                         
                         <label className="input-label" htmlFor="password">Password</label>
-                        <input className="auth-input" type="password" id="password" name="password" />
+                        <input className="auth-input" type="password" id="password" name="password" required />
                         
                         <button className="auth-button" type="submit">Go</button>
                     </form>

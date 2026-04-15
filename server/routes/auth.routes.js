7b3942dfaf9f1af76369556b3607d329d81812ef
Mekha0 <mehreenk767@gmail.com>
2026-04-15 01:35:49 -0400

    updated routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login, activateEmail } = require("../controllers/auth.controller");
+const { signup, login, activateEmail, resetPassword, activatePassword } = require("../controllers/auth.controller");
 
 
 // Signup route - mk
@@ -8,6 +8,11 @@ router.post("/signup", signup);
 
 // email activation route
 router.get("/activate/:token", activateEmail)
+
+// password reset
+
+router.post("/forgotPassword", resetPassword)
+router.post("/resetPassword", activatePassword)
 
 // Login endpoint
 router.post("/login", login);

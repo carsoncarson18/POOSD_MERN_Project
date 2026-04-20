44d511a30f909cbcf34d250df1610581a703be9c
Carson <carsondavie18@gmail.com>
2026-04-20 13:38:14 -0400

    claim/delete ingredient bug fix

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -33,13 +33,17 @@ export default function LoginPage() {
             // Saving token
             const token = response.data.token;
             localStorage.setItem("token", token);
+
+            // Save user id
+            localStorage.setItem("user", JSON.stringify({ id: response.data.user_id }));
+
             // Setting global header for session
             axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
             // Temporary success feedback, will change later
             navigate("/listingpage");
             console.log("Token:", response.data.token);
-            
+
         } catch (err: any) {
             let errorMessage = "Login failed";
 
@@ -48,7 +52,7 @@ export default function LoginPage() {
                 const fieldErrors = err.response.data.errors.fieldErrors;
                 const firstField = Object.keys(fieldErrors)[0];
                 errorMessage = fieldErrors[firstField][0];
-            } 
+            }
             else {
                 errorMessage = err.response?.data?.error || err.response?.data?.message || errorMessage;
             }
@@ -60,7 +64,7 @@ export default function LoginPage() {
     return (
         <div className="auth-page-wrapper">
             <SiteHeader />
-            
+
             <main className="auth-main">
                 <section className="auth-card">
                     <h1>Log In</h1>
@@ -75,27 +79,27 @@ export default function LoginPage() {
                             Account verified! You can now log in
                         </p>
                     )}
-                    
+
                     <form className="auth-form" onSubmit={handleLogin} noValidate>
                         <div className="input-group">
                             <label className="input-label" htmlFor="username">Username</label>
-                            <input 
-                                className="auth-input" 
-                                type="text" 
-                                id="username" 
-                                name="username" 
+                            <input
+                                className="auth-input"
+                                type="text"
+                                id="username"
+                                name="username"
                                 value={formData.username}
                                 onChange={handleChange}
                             />
                         </div>
-                        
+
                         <div className="input-group">
                             <label className="input-label" htmlFor="password">Password</label>
-                            <input 
-                                className="auth-input" 
-                                type="password" 
-                                id="password" 
-                                name="password" 
+                            <input
+                                className="auth-input"
+                                type="password"
+                                id="password"
+                                name="password"
                                 value={formData.password}
                                 onChange={handleChange}
                             />
@@ -103,7 +107,7 @@ export default function LoginPage() {
                                 Forgot password?
                             </button>
                         </div>
-                        
+
                         {error && (
                             <p className="auth-error">
                                 {error}
@@ -114,7 +118,7 @@ export default function LoginPage() {
                     </form>
                 </section>
             </main>
-            
+
             <SiteFooter />
         </div>
     );

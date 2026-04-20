131eb653079d1be7427e253d1c4d55494a940ffb
GeorgeAvdella <166246086+GeorgeAvdella@users.noreply.github.com>
2026-04-20 15:54:55 -0400

    Update neighborhood.validator.js

--- server/validators/neighborhood.validator.js
+++ server/validators/neighborhood.validator.js
@@ -3,7 +3,7 @@ const { z } = require("zod");
 const hoodNameSchema = z.string()
             .min(3,"Neighborhood name must be at least 3 characters")
             .max(16,"Neighborhood name must be at most 16 characters")
-            .regex(/^[a-zA-Z][a-zA-Z0-9_-]*$/, "Name must start with a letter and only contain letters, numbers, underscores, or hyphens")
+            .regex(/^[a-zA-Z][a-zA-Z0-9_ -]*$/, "Name must start with a letter and only contain letters, numbers, underscores, hyphens, or spaces")
 
 
 module.exports = { hoodNameSchema };

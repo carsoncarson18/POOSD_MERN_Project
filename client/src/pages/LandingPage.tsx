342061d44c5cd3c94ee5c20533d329cee96d533f
Mekha0 <mehreenk767@gmail.com>
2026-04-22 17:33:45 -0400

    flow and typos

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -25,15 +25,14 @@ export default function LandingPage() {
                                 backdropFilter:'blur(2px)',
                                 margin:'0 auto'
                             }}>
-                            <h1 style={{margin:0,color:'white'}}>What is Scraps?</h1>
+                            <h1 style={{margin:0,color:'white'}}>Welcome to the Neighborhood!</h1>
                             <p style={{width:'80%',margin:0,fontWeight:600, color: 'white'}}>
-                                Scraps is our solution to the problem of having 
-                                extra ingredients or food items that you do not 
-                                want to go to waste. <br/><br/> With Scraps, you can notify 
-                                your neighbors of any items you don’t want with a 
-                                corresponding expiration date and drop off location. 
-                                Scraps is also useful if you are in need of food items 
-                                rather than just for giving out food items.
+                                Scraps is our solution to having too many extra ingredients or 
+                                food items that you don't want to go to waste.<br/><br/> 
+                                With Scraps, you can post extra items with their expiration date 
+                                online and your neighbors can claim them. If you're short a few 
+                                items, check your Scraps feed; it might just have what you're 
+                                looking for!
                             </p>
                         </div>
                     </div>
@@ -43,11 +42,11 @@ export default function LandingPage() {
                             <section>
                                 <h2>Create an Account</h2>
                                 <p>
-                                    When you create an account, you will be prompted to 
-                                    enter your profile information like your name, phone 
-                                    number, and zip code. After logging in to your account, 
-                                    you will be redirected to your neighborhoods page where 
-                                    you can create or join communities for your area.
+                                    On the signup page, you'll be prompted to enter some
+                                    profile information like your first name and email. 
+                                    We'll send an email verifying your account and, after
+                                    following the instructions, you can log in and see your 
+                                    very own neighborhoods page.
                                 </p>
                             </section>
                             <section>
@@ -55,31 +54,34 @@ export default function LandingPage() {
                                 <p>
                                     Scraps organizes user zip codes into neighborhoods 
                                     so you can communicate with the people near you. 
-                                    If there is not already a neighborhood near where 
-                                    you live to join, you can create one. After entering 
-                                    a neighbor hood, you will be redirected to the 
-                                    listings page.
+                                    If there's not already a neighborhood near where 
+                                    you live to join, you can create one and name it. 
+                                    After entering a neighborhood, you'll be redirected 
+                                    to the neighborhood's listings page.
                                 </p>
                             </section>
                             <section>
                                 <h2>View Listings</h2>
                                 <p>
-                                    The listings page is where you can view the food 
-                                    items available to claim in your currently selected 
-                                    neighborhood. These listings will provide an image 
-                                    of the item, its name, its drop off location, and its 
-                                    expiration date. In the listings page, you also have 
-                                    the option to create your own listing, which will prompt 
-                                    you to enter the same type of information describes above.
+                                    The listings page is where you can view the claimable
+                                    food items posted by the members of the neighborhood. 
+                                    These listings will provide the item's image, name, 
+                                    description, quantity, category, and expiration date. 
+                                    You can filter based on category or just scroll through 
+                                    the entire page. On the flip side, you can also create 
+                                    your own listing on this page; just enter the same type 
+                                    of information described above and hit post!
                                 </p>
                             </section>
                             <section>
                                 <h2>Account Management</h2>
                                 <p>
-                                    At any time, you can update your profile and personal 
-                                    information when needed, and you can even manage list 
-                                    of your personal zip codes if you know you might visit 
-                                    more than one place at once.
+                                    At any time, you can update your own listings when
+                                    needed or even update the list of your personal zips.
+                                    If you're staying in an new area for a while, enter 
+                                    its zipcode and see if you can connect with the 
+                                    neighborhood through Scraps, the fantastic community-driven 
+                                    food-sharing platform!
                                 </p>
                             </section>
                         </div>
